458a0347cc9c5f1477abf2b77fc4cec14f61c3bf
Anton Widera <anton.widera@rwth-aachen.de>
2018-03-08 20:33:14 +0100

    Started Simple NLP algorithm; Label ChartType and Column work

--- server/controllers/nlp.js
+++ server/controllers/nlp.js
@@ -1,5 +1,9 @@
 let bookshelf = require('../config/bookshelf');
 let commands = require('../data/commands.json');
+let natural = require('natural');
+var config = require('../knexfile');
+var knex = require('knex')(config);
+var _ = require('lodash');
 
 function getDatasets() {
     return bookshelf.Model.extend({tableName: 'generic_dataset'}).fetchAll()
@@ -20,14 +24,143 @@ let functions = {
     }
 };
 
-function findCommand(dataset, input) {
-    // TODO replace with NLP logic
-    return {
-        function: 'plotHistogramOfColumn',
-        parameters: [
-            'age'
-        ]
+
+
+
+var tokenTypes = [{type: "operation", values: ["",""], function: extractOperation},{type: "Column", function: extractColumn}
+, {type: "Connector", values: ["of", "with"], function: extractConnector}
+, {type: "ChartType", values: ["Histogram", "Pie Chart"], function: extractChartType}];
+
+
+function extractOperation(state){
+        if(state.tokens[state.currentToken].matches()) {
+            // this layer matches for this token
+            state.currentToken++;
+            state.layer = 0;
+            findCommand(state);
+        } else {
+            // this layer does not match
+            state.layer++;
+            findCommand(state);
+        }
+}
+
+function extractColumn(state){
+    knex('human_resources__core_dataset').columnInfo().then(function(columnInfo) {
+        var currentToken = state.tokens[state.currentToken]
+        var result = _.find(columnInfo, function(o, i){
+            return i === currentToken;
+        });
+
+        if(result) {
+            // this layer matches for this token
+            state.tokens[state.currentToken] = {type: "Column", value: currentToken}
+            state.currentToken++;
+            state.layer = 0;
+            findCommand(state);
+        } else {
+            // this layer does not match
+            if(state.layer < searchFunction.length-1){
+                state.layer++;
+            } else {
+                state.layer = 0
+                state.currentToken++
+            }
+            findCommand(state);
+        }
+    });
+}
+
+function extractChartType(state){
+    var currentToken = state.tokens[state.currentToken]
+    let possibleTypes = ["Histogram"];
+
+    var result = _.find(possibleTypes, function(o){
+        return o === currentToken;
+    });
+
+    if(result) {
+        // this layer matches for this token
+        state.tokens[state.currentToken] = {type: "ChartType", value: currentToken}
+        state.currentToken++;
+        state.layer = 0;
+        findCommand(state);
+    } else {
+        // this layer does not match
+        if(state.layer < searchFunction.length-1){
+            state.layer++;
+        } else {
+            state.layer = 0
+            state.currentToken++
+        }
+        findCommand(state);
     }
+
+
+}
+
+function extractConnector(state){
+
+}
+
+let searchFunction = [
+    extractColumn,
+    extractChartType
+];
+
+
+function findCommand(state) {
+    if(state.currentToken < state.tokens.length && state.layer < searchFunction.length) {
+        searchFunction[state.layer](state);
+    } else {
+        state.callback(state)
+    }
+}
+
+function findCommands(dataset, input, callback) {
+    // TODO replace with NLP logic
+
+
+
+
+/*
+
+    _.forEach(tokenTypes, function(type){
+
+    });
+
+
+    knex('human_resources__core_dataset').columnInfo().then(function(columnInfo) {
+
+        _.forEach(tokenizedInput, function(token){
+            var result = _.find(columnInfo, function(o, i){
+                return i === token;
+            });
+            if(result !== undefined){
+                info["ColumnSelector"] = { operator: 'ColumnSelector', value: token};
+            }
+        });
+
+        var result = _.find(info, function(o){
+            if(o.operator === 'ColumnSelector'){
+                return o;
+            }
+        });
+
+        callback({
+            function: 'plotHistogramOfColumn',
+            parameters: [
+                result.token
+            ]
+        });
+
+    });*/
+    /*Expression:Operation - Select, Plot, Make
+
+    Expression:Dataset - employee
+
+    Expression:ChartType - histogram, pie chart, bar chart, line chart, scatter plot
+    Indicator: as a*/
 }
 
 /**
@@ -47,12 +180,43 @@ exports.handleInput = function (req, res) {
 
         let dataset = req.body.dataset;
 
+        var tokenizer = new natural.WordTokenizer();
+        let tokenizedInput = tokenizer.tokenize(req.body.input);
+
+        let initState = {
+            tokens: tokenizedInput,
+            layer: 0,
+            currentToken: 0,
+            callback: state => {
+                debugger;
+                let bestMatch = {
+                    function: 'plotHistogramOfColumn',
+                    parameters: [
+                    ]
+                };
+
+                _.forEach(state.tokens, function(token){
+                    if(token.type === "Column"){
+                        bestMatch.parameters.push(token.value);
+                    }
+                });
+                functions[bestMatch.function](dataset, bestMatch.parameters, data =>
+                    // Send the data back to the client
+                    res.send({data: data})
+                )
+            }
+        };
+
+        findCommand(initState);
+
         // Use NLP to find the right command
-        let command = findCommand(dataset, req.body.input);
+        /*findCommand(dataset, req.body.input, command =>
+            functions[command.function](dataset, command.parameters, data =>
+                // Send the data back to the client
+                res.send({data: data})
+            )
+        );*/
         // Query the needed data and transform them into the dataformat for plotly.js
-        functions[command.function](dataset, command.parameters, data =>
-            // Send the data back to the client
-            res.send({data: data})
-        );
+
     })
 };
